d417c46c536e7373371782c833c8b607a9eb76ff
Tyler Gunn <tylergunn@reedssports.com>
2025-01-11 18:34:43 -0600

    test

--- gemini.py
+++ gemini.py
@@ -38,6 +38,22 @@ def upload_file_to_gemini(file_path):
         print(f"Error uploading file: {e}")
         return None, None
 if __name__ == "__main__":
+  form_prompt = """ Generate a google from with 5 multiple choice questions.
+Using the following JSON, create a Google Form. The JSON should be formatted as follows:
+Using the following resource https://developers.google.com/forms/api/reference/rest/v1/forms/batchUpdate#Request create google forms.
+                    **Instructions**:  
+                    - If the question is multiple-choice, use the `choiceQuestion` block with a dynamic `type`. The type should be one of the following:
+                        - `RADIO`: If the user can select only one option.
+                        - `CHECKBOX`: If the user can select multiple options.
+                        - `DROP_DOWN`: If the user selects one option from a dropdown.
+                    - If the question requires a text answer, use the `textQuestion` block.
+                    - For each question, include an image URI if available using the `image` key. If no image is available, omit the `image` key.
+                    - Ensure that the correct answer is specified, and add points for grading.
+                    
+                    Return *only* the JSON object. Do not include any backticks (```), code fences, or explanatory text.
+"""
+  response = geminiQuery(form_prompt)
+  print(response.text)
 #     response = geminiQuery("""Generate JSON for creating questions in a Google Form quiz based on the Minnesota Comprehensive Assessments (MCA) state standards for 8th-grade math. Create 3 multiple-choice questions. For each question, specify the correct answer. The JSON should follow this format:
 # {
 #   "requests": [
@@ -76,4 +92,4 @@ if __name__ == "__main__":
 #   ]
 # }""")
 #     print(response.text)
-    upload_file_to_gemini('Staff Handbook 24-25.pdf')
+    # upload_file_to_gemini('Staff Handbook 24-25.pdf')
